6f8ef2a00a9ffd1265d64cb35c870466ad1fa666
Alex Mckay <0900959m@student.gla.ac.uk>
2013-10-21 03:21:38 +0100

    started on javaFX version. very basic coordinate grabber as of now

---
+++ JavaFX v1/src/Coord.java
@@ -0,0 +1,30 @@
+
+public class Coord {
+
+	double x, y;
+	
+	public Coord(){
+		x=0;
+		y=0;
+	}
+	
+	public void setX(double xx){
+		x=xx;
+	}
+	
+	public void setY(double yy){
+		y=yy;
+	}
+	
+	public double getX(){
+		return x;
+	}
+	
+	public double getY(){
+		return y;
+	}
+	
+	public void printValues(){
+    	System.out.printf("x = %.0f y = %.0f \n", x, y);
+	}
+}
